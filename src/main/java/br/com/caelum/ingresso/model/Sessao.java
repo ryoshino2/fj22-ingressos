a0dd2237bea46dc2f89ff9d5a74d4d862ed92c15
rafael <rafayoshino02@gmail.com>
2019-02-25 21:12:50 -0300

    Incluindo preco na sala, filme e sessao

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -18,6 +20,15 @@ public class Sessao {
 	private Sala sala;
 	@ManyToOne
 	private Filme filme;
+	private BigDecimal preco;
+
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
 
 	public Sessao() {
 	}
@@ -26,6 +37,7 @@ public class Sessao {
 		this.horario = horario;
 		this.sala = sala;
 		this.filme = filme;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public LocalTime getHorarioTermino() {
---
+++ src/test/java/br/com/caelum/ingresso/model/SessaoTest.java
@@ -0,0 +1,20 @@
+package br.com.caelum.ingresso.model;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class SessaoTest {
+	@Test
+	public void oPrecoDaSessaoDeveSerIgualASomaDoPrecoDaSalaMaisPrecoDoFilme() {
+		Sala sala = new Sala ("Sala 3d", new BigDecimal(10.0));
+		Filme filme = new Filme ("Dragon Ball", Duration.ofMinutes(120),"Desenho", new BigDecimal(10.0));
+		BigDecimal somaDosPrecosDaSalaEFilme = sala.getPreco().add(filme.getPreco());
+		Sessao sessao = new Sessao (LocalTime.parse ("10:00:00"), filme, sala);
+		Assert.assertEquals(somaDosPrecosDaSalaEFilme, sessao.getPreco());
+	}
+}
